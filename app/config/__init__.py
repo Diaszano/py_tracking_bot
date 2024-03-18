58dfadae47471b4d51120eca93b155a24b215f62
Diaszano <luucas.dsantos@gmail.com>
2024-03-18 20:44:58 -0300

    fix: corrigida a importação do módulo enviroments

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -3,6 +3,6 @@ This package is reposable by configurations.
 
 It provides all the necessary settings for the system to run successfully.
 """
-import enviroments
+from . import enviroments
 
 __all__ = ('enviroments',)
