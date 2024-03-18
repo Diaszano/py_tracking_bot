2f6b66a0937c7b7a5a682e28a0ab06f511bd81f4
Diaszano <luucas.dsantos@gmail.com>
2024-03-18 20:36:44 -0300

    fix: corrigido e documentado o arquivo de inicialização do pacote

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,4 +1,11 @@
-from .connection import Engine
-from .schema import Base
+"""
+This package is responsible for configuring the database.
 
-__all__ = (Engine, Base)
+It provides all the necessary configurations to use the database in the system.
+"""
+from sqlalchemy.ext.asyncio import AsyncSession as Session
+
+from .connection import Engine
+from .schema import Base, Order, Tracking, User
+
+__all__ = ('Base', 'Engine', 'Order', 'Session', 'Tracking', 'User')
