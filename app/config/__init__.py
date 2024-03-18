9bc8cf16368a4236d21fbafe36fa25a88a3e660d
Diaszano <luucas.dsantos@gmail.com>
2024-03-18 20:33:47 -0300

    feat: configurado o arquivo de inicialização do pacote

---
+++ app/config/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package is reposable by configurations.
+
+It provides all the necessary settings for the system to run successfully.
+"""
+import enviroments
+
+__all__ = ('enviroments',)
