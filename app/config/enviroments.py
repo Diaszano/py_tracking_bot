e43b353c0b10680f10c340319fea61a03d3eb8c6
Diaszano <luucas.dsantos@gmail.com>
2024-03-18 17:45:20 -0300

    chore: movidos arquivos para a pasta app

---
+++ app/config/enviroments.py
@@ -0,0 +1,33 @@
+"""
+This module is responsible for loading system environment settings.
+
+It provides functionalities to load and manage environment settings, allowing for easy and flexible system configuration.
+"""
+from urllib.parse import ParseResult
+
+from environs import Env
+from marshmallow.validate import Length, Range
+
+__env = Env(expand_vars=True, eager=False)
+__env.read_env()
+
+with __env.prefixed('TELEGRAM_'):
+    with __env.prefixed('API_'):
+        TELEGRAM_API_ID: int = __env.int('ID', validate=Range(min=1))
+        TELEGRAM_API_HASH: str = __env.str('HASH', validate=Length(min=10))
+    with __env.prefixed('BOT_'):
+        TELEGRAM_BOT_TOKEN: str = __env.str('TOKEN', validate=Length(min=10))
+
+with __env.prefixed('DATABASE_'):
+    DATABASE_URL: ParseResult = __env.url(
+        'URL', schemes=['postgresql+asyncpg'], require_tld=False
+    )
+
+
+__env.seal()
+
+if __name__ == '__main__':
+    print(TELEGRAM_API_ID, type(TELEGRAM_API_ID))
+    print(TELEGRAM_API_HASH, type(TELEGRAM_API_HASH))
+    print(TELEGRAM_BOT_TOKEN, type(TELEGRAM_BOT_TOKEN))
+    print(DATABASE_URL, type(DATABASE_URL))
