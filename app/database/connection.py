8aec4d9f49baac5d7f7bca58fee5c5095de4643f
Diaszano <luucas.dsantos@gmail.com>
2024-03-21 12:50:21 -0300

    fix(🐛): corrigida a importação do pacote quando vai ser chamado pelo alembic

--- app/database/connection.py
+++ app/database/connection.py
@@ -3,7 +3,10 @@ This module is responsible for connecting to the database.
 
 Provides functionalities to create engine for the database.
 """
-from config.enviroments import DATABASE_URL
+try:
+    from config.enviroments import DATABASE_URL
+except ImportError:
+    from ..config.enviroments import DATABASE_URL
 from sqlalchemy.ext.asyncio import create_async_engine
 
 Engine = create_async_engine(DATABASE_URL.geturl())
