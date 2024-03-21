e56af86727b46f07f71c4e41f873bfebf471875b
Diaszano <luucas.dsantos@gmail.com>
2024-03-21 13:01:20 -0300

    feat(🚀): adiconado a criação de logs em arquivo

--- app/utils/log.py
+++ app/utils/log.py
@@ -4,4 +4,4 @@ from config.enviroments import LOG_LEVEl
 from loguru import logger
 
 logger.add(sink=stderr, level=LOG_LEVEl, enqueue=True)
-logger.add("PyTrackingBot_{time}.log", enqueue=True, serialize=True)
+logger.add('PyTrackingBot_{time}.log', enqueue=True, serialize=True)
