d0692069bcf953e45d9a81d0995b7479c8d182c3
Diaszano <luucas.dsantos@gmail.com>
2024-03-20 21:36:05 -0300

    feat: corrigido o caminho relativo do arquivo das envs

--- app/database/connection.py
+++ app/database/connection.py
@@ -3,8 +3,7 @@ This module is responsible for connecting to the database.
 
 Provides functionalities to create engine for the database.
 """
+from config.enviroments import DATABASE_URL
 from sqlalchemy.ext.asyncio import create_async_engine
-
-from ..config.enviroments import DATABASE_URL
 
 Engine = create_async_engine(DATABASE_URL.geturl())
