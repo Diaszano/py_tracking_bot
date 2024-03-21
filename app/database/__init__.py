44f2d78bf030e26d313e749affda407646a7cda0
Diaszano <luucas.dsantos@gmail.com>
2024-03-21 12:52:06 -0300

    fix(♻️): removida importações de modulos não existentes

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -6,6 +6,5 @@ It provides all the necessary configurations to use the database in the system.
 from sqlalchemy.ext.asyncio import AsyncSession as Session
 
 from .connection import Engine
-from .schema import Base, Order, Tracking, User
 
-__all__ = ('Base', 'Engine', 'Order', 'Session', 'Tracking', 'User')
+__all__ = ('Engine', 'Session')
