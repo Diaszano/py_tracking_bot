1fe649fc4f6cf698413c652ba74cadf1c1cc610a
Diaszano <luucas.dsantos@gmail.com>
2024-03-23 20:49:42 -0300

    fix(🐛): corrigido para não mostrar mais a url do banco de dados

--- alembic/env.py
+++ alembic/env.py
@@ -8,8 +8,6 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from app import DATABASE_URL, Base
 
-print(DATABASE_URL, Base)
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
