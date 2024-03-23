ac81795cf513b5337794494b0690f6f6574f6ffe
Diaszano <luucas.dsantos@gmail.com>
2024-03-23 20:57:06 -0300

    docs(📝): colocada a documentação do código em pt_br

--- app/config/enviroments.py
+++ app/config/enviroments.py
@@ -1,37 +1,38 @@
 """
-This module is responsible for loading system environment settings.
+Este módulo é responsável pelo carregamento das variáveis de ambiente do sistema.
 
-It provides functionalities to load and manage environment settings, allowing for easy and flexible system configuration.
+Ele contém funcionalidades para carregar e gerenciar as variáveis de ambiente,
+simplificando e tornando mais flexível a configuração do sistema.
 """
+
 from logging import INFO
 from urllib.parse import ParseResult
 
 from environs import Env
 from marshmallow.validate import Length, Range
 
+# Inicialização do ambiente
 __env = Env(expand_vars=True, eager=False)
 __env.read_env()
 
+# Configurações das variáveis de ambiente relacionadas ao Telegram
 with __env.prefixed('TELEGRAM_'):
     with __env.prefixed('API_'):
         TELEGRAM_API_ID: int = __env.int('ID', validate=Range(min=1))
         TELEGRAM_API_HASH: str = __env.str('HASH', validate=Length(min=10))
     with __env.prefixed('BOT_'):
         TELEGRAM_BOT_TOKEN: str = __env.str('TOKEN', validate=Length(min=10))
-        TELEGRAM_BOT_NAME: str = __env.str('NAME', validate=Length(1))
+        TELEGRAM_BOT_NAME: str = __env.str('NAME', validate=Length(min=1))
 
+# Configuração da URL do banco de dados
 with __env.prefixed('DATABASE_'):
     DATABASE_URL: ParseResult = __env.url(
         'URL', schemes=['postgresql+asyncpg'], require_tld=False
     )
 
+# Configuração do nível de log
 with __env.prefixed('LOG_'):
-    LOG_LEVEl: int = __env.log_level('LEVEL', INFO)
+    LOG_LEVEL: int = __env.log_level('LEVEL', INFO)
 
+# Sela o ambiente após a leitura das variáveis de ambiente
 __env.seal()
-
-if __name__ == '__main__':
-    print(TELEGRAM_API_ID, type(TELEGRAM_API_ID))
-    print(TELEGRAM_API_HASH, type(TELEGRAM_API_HASH))
-    print(TELEGRAM_BOT_TOKEN, type(TELEGRAM_BOT_TOKEN))
-    print(DATABASE_URL, type(DATABASE_URL))
