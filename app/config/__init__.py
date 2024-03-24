1c309f1647549afa756749cd3498919bf5327c1c
Diaszano <luucas.dsantos@gmail.com>
2024-03-24 20:32:10 -0300

    fix(🐛): removida a importação do enviroments

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -3,7 +3,3 @@ Este pacote é responsável pelas configurações.
 
 Ele fornece todas as configurações necessárias para o sistema funcionar com sucesso.
 """
-
-from . import environments
-
-__all__ = ('environments',)
